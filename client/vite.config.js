6fa0f0022fac1f9f0f8230949434c35fa6556be8
Sreenadh2000 <sreenadh056@gmail.com>
2026-03-15 22:39:38 +0530

    removed for build checkup

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,16 +3,14 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler"]],
-      },
-    }),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
   server: {
     port: 9898, // change the port
     open: true, // automatically open this
+  },
+  // Add this to ensure build stability on Render
+  build: {
+    outDir: "dist",
+    sourcemap: false, // Reduces memory usage during build
   },
 });
